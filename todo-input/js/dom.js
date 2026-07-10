397f6161a84108a62317d58336f9abacd49b02f8
Apex5621 <hramow.ar.217@gmail.com>
2026-07-10 15:00:57 +0300

    Todo Input added

---
+++ todo-input/js/dom.js
@@ -0,0 +1,105 @@
+import { todoKeys } from './constants.js'
+import { createTodo, editTodoById, completeTodoById, deleteTodoById } from './service.js'
+import { setTodosToLocalStorage } from './storage.js'
+
+// При помощи метода querySelector получаем элементы .form, .input и .todos
+const formElement = document.querySelector('.form')
+const inputElement = document.querySelector('.input')
+const todosElement = document.querySelector('.todos')
+
+// Создаем функцию createTodoElement(text), которая будет создавать todo в виде разметки
+const createTodoElement = todo => {
+  const todoElement = document.createElement('li')
+  const todoText = document.createElement('input')
+
+  todoElement.classList.add('todo')
+  todoElement.dataset.id = todo[todoKeys.id]
+  todoText.classList.add('todo-text')
+  todoText.type = 'text'
+  todoText.value = todo[todoKeys.text]
+  todoText.readOnly = true
+  todoText.tabIndex = -1
+  todoElement.innerHTML = `
+    <div class="todo-actions">
+      <button class="button-edit button">&#x270E;</button>
+      <button class="button-complete button">&#10004;</button>
+      <button class="button-delete button">&#10006;</button>
+    </div>
+  `
+  todoElement.prepend(todoText)
+  return todoElement
+}
+
+export const renderTodos = todos => {
+  todosElement.innerHTML = ''
+  todos.forEach(todo => {
+    const todoElement = createTodoElement(todo)
+    if (todo[todoKeys.is_completed]) {
+      todoElement.classList.add('completed')
+    }
+    todosElement.prepend(todoElement)
+  })
+}
+
+// Создаем функцию handleCreateTodo(todos, text), которая будет вызывать createTodo и createTodoElement
+const handleCreateTodo = (todos, text) => {
+  const todo = createTodo(todos, text)
+  const todoElement = createTodoElement(todo)
+  setTodosToLocalStorage(todos)
+  todosElement.prepend(todoElement)
+}
+
+const saveTodoText = (todos, todo, todoText) => {
+  const todoId = Number(todo.dataset.id)
+  const text = todoText.value.trim()
+  if (!text) return
+  const updatedTodo = editTodoById(todos, todoId, text)
+  if (!updatedTodo) return
+  setTodosToLocalStorage(todos)
+  todoText.value = updatedTodo[todoKeys.text]
+  todoText.readOnly = true
+  todoText.tabIndex = -1
+}
+
+export const initTodoHandlers = todos => {
+  ;(formElement.addEventListener('submit', event => {
+    event.preventDefault()
+    const text = inputElement.value.trim()
+    if (!text) return
+    handleCreateTodo(todos, text)
+    inputElement.value = ''
+  }),
+    todosElement.addEventListener('click', ({ target }) => {
+      const todo = target.closest('.todo')
+      if (!todo) return
+      const todoId = Number(todo.dataset.id)
+      if (target.matches('.button-edit')) {
+        const todoText = todo.querySelector('.todo-text')
+        if (!todoText.readOnly) {
+          saveTodoText(todos, todo, todoText)
+          return
+        }
+        todoText.readOnly = false
+        todoText.tabIndex = 0
+        todoText.focus()
+        todoText.select()
+      }
+      if (target.matches('.button-complete')) {
+        completeTodoById(todos, todoId)
+        setTodosToLocalStorage(todos)
+        todo.classList.toggle('completed')
+      }
+      if (target.matches('.button-delete')) {
+        deleteTodoById(todos, todoId)
+        setTodosToLocalStorage(todos)
+        todo.remove()
+      }
+    }),
+    todosElement.addEventListener('keydown', ({ target, key }) => {
+      if (!target.matches('.todo-text')) return
+      if (key !== 'Enter') return
+      const todo = target.closest('.todo')
+      if (!todo) return
+      saveTodoText(todos, todo, target)
+    }))
+}
